7e586a1ee8f3f37570bd85c83c9f2aafcefd3209
Kovalev1323 <Kovalev1323@mail.ru>
2024-12-17 22:48:04 +0300

    Add JavaDoc
    taet

---
+++ JavaDoc/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.graphs","l":"GraphApp"},{"p":"com.graphs","l":"GraphRenderer"},{"p":"com.graphs","l":"GraphUtils"},{"p":"com.graphs","l":"Launcher"}];updateSearchResults();
